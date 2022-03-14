200dd1ee3a2297bb8f55ec960f2167411c64504e
rowl <airowl.gasga@gmail.com>
2022-03-14 16:42:13 +0100

    completato milestone 2 aggiunto elementi posts nel DOM

--- js/main.js
+++ js/main.js
@@ -220,4 +220,43 @@ const posts = [
 
 const postsContainer = document.querySelector('.posts-list');
 
-//! stampiamo i post con i dati dell'a
+//! stampiamo i post con i dati dell'array di oggetti
+
+//* creiamo un ciclo for each per stampare tutti i posts con il codice HTML insieme ai suoi dati
+posts.forEach((element) => {
+    
+    postsContainer.innerHTML += `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${element['author']['image']}" alt="${element['author']['name']}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${element['author']['name']}</div>
+                        <div class="post-meta__time">${element['created']}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${element['content']}</div>
+            <div class="post__image">
+                <img src="${element['media']}" alt="${element['author']['name']}">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${element['likes']}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `;
+
+});
+
