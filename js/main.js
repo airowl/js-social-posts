d7e10f79d030bf0daeb1dfd477ad7a2606c93129
rowl <airowl.gasga@gmail.com>
2022-03-14 18:33:51 +0100

    milestone 3 al click incrementiamo il counter likes e il bottone cambia colore

--- js/main.js
+++ js/main.js
@@ -245,18 +245,33 @@ posts.forEach((element) => {
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button" data-postid="1">
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${element['likes']}</b> persone
+                        Piace a <b id="like-counter-${element['id']}" class="js-likes-counter">${element['likes']}</b> persone
                     </div>
                 </div> 
             </div>            
         </div>
     `;
-
 });
+
+//! aggiungiamo una funziona al click della sezione Mi piace
+const likeButton = document.querySelectorAll('a.like-button');
+
+console.log(likeButton);
+
+for (let i = 0; i < likeButton.length; i++) {
+    likeButton[i].addEventListener('click', function() {
+        this.classList.toggle('js-like-button');
+        
+        document.getElementById('like-counter-' + posts[i].id).innerHTML = ++posts[i].likes;
+    });
+};
+
+
+
 
